3b468caff848b0bedf4a805168821a6e320b9dca
amwetgfcu <183859519@qq.com>
2019-01-22 17:14:55 +0800

    gshop
    这个是VUe项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script src="../../Desktop/code/gshop-server_final/bin/www"></script>
+<template>
+ <div id="app">
+   <router-view></router-view>
+   <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+ </div>
+</template>
+
+<script>
+    import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+    export default{
+      components:{
+        FooterGuide
+      }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
